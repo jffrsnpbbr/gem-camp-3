77bd03954a0409491a64bfac76b91fe2ee0273a9
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-03-22 11:47:58 +0800

    update post controller then add view edit show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,21 @@ class PostsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :content))
+      redirect_to posts_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
