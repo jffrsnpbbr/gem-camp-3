bb8e46d0a90a35226a6f98f378577e9449b4abfa
jefferson.pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-03-28 13:23:45 +0800

    change root directory to posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'welcome#index'
+  root 'posts#index'
 
   resources :posts do
     resources :comments, except: :show
   end
-
+  
   resources :categories
 end
