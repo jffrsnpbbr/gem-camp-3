3966e31f39a0b59bf3790029567efdb86b95687a
jefferson.pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-03-30 14:16:43 +0800

    add auto translation en to zh-cn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if params[:lang] && I18n.available_locales.include?(params[:lang].to_sym)
+      session[:lang] = params[:lang]
+    end
+
+    I18n.locale = session[:lang] || I18.default_locale
+  end
 end
