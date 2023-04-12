099351f21e09726fd5492517cac75a9e0b1f4a6a
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-12 16:01:04 +0800

    create model address regions and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_022245) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_071612) do
+  create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
