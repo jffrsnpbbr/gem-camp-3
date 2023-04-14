12d30cfb303ea9a4ff25e63ec3c71aa9c1d9b5fe
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-14 19:14:35 +0800

    create api address cities

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   namespace :api do
     namespace :v1 do
       resources :regions, only: [:index, :show], defaults: { format: :json } do
-        resources :provinces, only: [:index, :show], defaults: { format: :json }
+        resources :provinces, only: [:index, :show], defaults: { format: :json } do
+          resources :cities, only: [:index, :show], defaults: { format: :json }
+        end
       end
-      resources :provinces, only: [:index, :show], defaults: { format: :json }
+      resources :provinces, only: [:index, :show], defaults: { format: :json } do
+        resources :cities, only: [:index, :show], defaults: { format: :json }
+      end
+      resources :cities, only: [:index, :show], defaults: { format: :json }
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root 'welcome#index'
 
   resources :posts do
