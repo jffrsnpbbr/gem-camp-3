bfc78374ffe8bfc41188b738596733195413f9eb
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-14 17:34:54 +0800

    create api address regions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: [:index, :show], defaults: { format: :json }
+    end
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
