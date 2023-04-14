51f39da49a86a00caa924a687e652c75dd005586
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-14 17:50:38 +0800

    create api address provinces

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   namespace :api do
     namespace :v1 do
-      resources :regions, only: [:index, :show], defaults: { format: :json }
+      resources :regions, only: [:index, :show], defaults: { format: :json } do
+        resources :provinces, only: [:index, :show], defaults: { format: :json }
+      end
+      resources :provinces, only: [:index, :show], defaults: { format: :json }
     end
   end
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
